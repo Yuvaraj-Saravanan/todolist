02002d147f9ef2bb34b613f3b8778fc607abd81d
Yuvaraj-Saravanan <yuvarajs21ec@psnacet.edu.in>
2024-01-27 12:16:30 +0530

    todolist created

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var inp=document.getElementById("new")
+var listcontainer=document.getElementById("list-container")
+function update(){
+    newitem=document.createElement("li")
+    newitem.innerHTML=inp.value
+    listcontainer.append(newitem)
+    buton=document.createElement("button")
+    buton.textContent="X"
+    buton.setAttribute("class","btn")
+    buton.setAttribute("onclick","del(event)")
+    newitem.append(buton)
+    inp.value=null
+    store()
+}
+function del(){
+    event.target.parentElement.remove()
+    store()
+}
+function store(){
+    localStorage.setItem("data",listcontainer.innerHTML)
+}
+function show(){
+    listcontainer.innerHTML=localStorage.getItem("data")
+}
+show()
